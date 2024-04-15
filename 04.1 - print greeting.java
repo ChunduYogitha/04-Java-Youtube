b81240bc9dc0c80484a82ec6f6002efef632def7
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-04-15 12:16:44 +0530

    04.1 - print greeting.java

---
+++ 04.1 - print greeting.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args){
+      greeting();  
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
+/*
+Hello World
+*/
