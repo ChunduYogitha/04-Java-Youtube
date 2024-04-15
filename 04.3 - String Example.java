dbbc1813e7a531b01bbc79330b56380fdd57ce57
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-04-15 13:44:01 +0530

    04.3 - String Example.java

---
+++ 04.3 - String Example.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet() {
+        String greeting = "How are you";
+        return greeting;
+    }
+}
+/*
+How are You
+*/
