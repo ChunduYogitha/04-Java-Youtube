39be5262da10672b3fad27b53920c7312913164d
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-04-17 11:50:38 +0530

    04.4 - parameters.java

---
+++ 04.4 - parameters.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+      int ans =  sum(10,25); 
+      System.out.println(ans);
+    }
+    static int sum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+}
+
+/*
+  30
+*/
