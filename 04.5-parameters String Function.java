2578b5b09e4fd6d9a69be5079263efe3b223a5d3
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-04-17 14:34:58 +0530

    04.5-parameters String Function.java

--- 04.5-parameters String Function.java
+++ 04.5-parameters String Function.java
@@ -13,3 +13,22 @@ public class Main{
   Hello Yogitha
 */
 
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name = sc.next();
+        String text = greet(name);
+        System.out.println(text);
+    }
+    static String greet(String name){
+        String message = "Hello " + name;
+        return message;
+    }
+}
+/*
+Enter your name : Yogitha
+Hello Yogitha
+*/
