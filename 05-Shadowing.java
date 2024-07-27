3af071ff360743bf79400ad03d692c40b1fd5db1
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-07-27 11:58:35 +0530

    05-Shadowing.java

---
+++ 05-Shadowing.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+  static  int x = 90;
+    public static void main(String[] args){
+        System.out.println(x);
+        int x = 20;
+        System.out.println(x);
+        num();
+    }
+    static void num(){
+        System.out.println(x);
+    }
+}
+OUTPUT: 
+        90
+        20
+        90
+        
+        === Code Execution Successful ===
