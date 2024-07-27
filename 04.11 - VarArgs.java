77a1c7114ecead3af09abe9e9d55a2a63c9db03c
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2024-07-27 13:23:33 +0530

    04.11 - VarArgs.java

---
+++ 04.11 - VarArgs.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        fun(1,2,3,4,5,65);
+    }
+    static void fun(int...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
+OUTPUT
+[1, 2, 3, 4, 5, 65]
+
+=== Code Execution Successful ===
+
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        multiple(1,2,"Java","Python");
+    }
+    static void multiple(int a, int b, String...v){
+        
+    }
+}
